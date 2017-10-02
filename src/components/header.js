053bdd3d20ddf0c960f45bb132716db739d6419a
biodun <biodunch@gmail.com>
2017-10-02 19:29:16 +0100

    fix tiny responsive err

--- src/components/header.js
+++ src/components/header.js
@@ -3,14 +3,14 @@ import { styles } from '../styles/styles';
 
 const Header = (props) => {
     return (
-        <div style={styles.header} className='row'>
+        <header style={styles.header} className='col-md-12'>
             <div className='col-md-2 headerText' style={styles.brand} >
                 <h2 style={styles.headerText}>tuteria</h2>
             </div>
-            <div className='col-md-6' style={{paddingTop:'22px'}} >
-                <p style={styles.headerText}>General Mathematics Test</p>
+            <div className='col-md-6 headerText' style={{paddingTop:'22px'}} >
+                <p>General Mathematics Test</p>
             </div>
-        </div>
+        </header>
     )
 }
 
